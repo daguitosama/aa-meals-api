04390db517729fee8151417243c437894ace9fa4
d4g0 <tocarralero@gmail.com>
2021-08-25 09:04:39 -0400

    + helmet

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,12 @@ const PORT = process.env.PORT || 3010;
 import Express from 'express';
 const app = Express();
 import router from '~/server/router';
-import { apiLimiter } from '~/server/middleware/index.js'
+import { apiLimiter } from '~/server/middleware/index.js';
+import helmet from 'helmet';
+
 
 // general middlewares
-app.use(apiLimiter);
+app.use(apiLimiter,helmet());
 // api routes
 app.use('/api/', router);
 
--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -1,4 +1,7 @@
 import rateLimit from 'express-rate-limit';
+
+
+
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
 // app.set('trust proxy', 1);
