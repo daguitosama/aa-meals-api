e9a70d27050b1c5c853b98d2935257d2f6f4264e
d4g0 <tocarralero@gmail.com>
2021-08-25 06:20:13 -0400

    baseline

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _t = require("./t");
+
+(0, _t.sayAjas)();
---
+++ dist/t.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.sayAjas = sayAjas;
+
+function sayAjas() {
+  console.log('ajas');
+}
