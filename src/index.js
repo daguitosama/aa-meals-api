c1bcb19e91e53c09752055283f6a2874a4fe9463
d4g0 <tocarralero@gmail.com>
2022-08-27 01:43:08 -0400

    [clean] old alive bot aproach clean notify deps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import { spinUpServer } from "server";
-import { initNotifier } from "notifications";
 import { bot } from "~/bot/bot_api_service";
 spinUpServer();
-// initNotifier();
 bot.configure();
