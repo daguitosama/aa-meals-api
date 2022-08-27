1f1b6a74c14aa3ac676a51142ca17e5a069c0649
d4g0 <tocarralero@gmail.com>
2022-08-27 10:09:03 -0400

    lint

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,25 @@
 const PORT = process.env.API_PORT || 3010;
-import Express from 'express';
+import Express from "express";
 const app = Express();
-import router from '~/server/router';
-import { apiLimiter } from '~/server/middleware/index.js';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import router from "~/server/router";
+import { apiLimiter } from "~/server/middleware/index.js";
+import helmet from "helmet";
+import morgan from "morgan";
 
-if(process.env.NODE_ENV != 'production'){
-    console.log('env development detected')
-    app.use(morgan(':method :url :status  - :response-time ms'))
+if (process.env.NODE_ENV != "production") {
+  app.use(morgan(":method :url :status  - :response-time ms"));
 }
 
-// app settings 
-app.set('trust proxy', 1); // behind proxy setting
+// app settings
+app.set("trust proxy", 1); // behind proxy setting
 // general middlewares
-app.use(apiLimiter,helmet());
+app.use(apiLimiter, helmet());
 app.use(Express.json());
 // api routes
-app.use('/api/', router);
+app.use("/api/", router);
 
 export function spinUpServer() {
-    app.listen(PORT, () => { console.log('app listening at port:' + PORT) })
+  app.listen(PORT, () => {
+    console.log("app listening at port:" + PORT);
+  });
 }
-
-
